578d859bd19b250ae4027c8a3a25f728e150555d
Miguel Julio <fitfab@yahoo.com>
2016-04-20 00:18:46 -0400

    add http-proxy and rename file bundle

--- server.js
+++ server.js
@@ -53,24 +53,3 @@ proxy.on('error', function(e) {
 // Run the server
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
 
-
-
-
-
-
-// *****************************************
-// 'use strict';
-
-// const express = require('express');
-
-// // Constants
-// const PORT = 8080;
-
-// // App
-// const app = express();
-// app.get('/', function (req, res) {
-//   res.send('Hello world\n');
-// });
-
-// app.listen(PORT);
-// console.log('Running on http://localhost:' + PORT);
